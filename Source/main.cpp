5adfc86f056f33395fee93da445bad50b30c7bad
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2026-03-12 17:28:16 -0700

    Update main to use new functions

--- Source/main.cpp
+++ Source/main.cpp
@@ -21,15 +21,17 @@ int main(int, char **)
     const auto macbethResolution = MacbethResolution::Screen;
     const auto [height, width]   = MacbethResolutions[macbethResolution];
 
-    auto       rgbImage     = create_macbeth_colorchecker_data<Image1D_8U>(macbethResolution);
-    auto       macbethImage = CreateMacbethImage<Image1D_8U>(macbethResolution);
-    const auto jpegResult   = save_image(macbethImage, "/home/jaimerios/Pictures/MacbethImageRGB.jpg");
+    auto macbethImage = CreateMacbethImage<Image1D_8U>(macbethResolution);
+
+    const auto jpegResult = save_image(macbethImage, "/home/jaimerios/Pictures/MacbethImageRGB.jpg");
     std::cout << "JPEG Image save result: " << (jpegResult ? "Success\n" : "Fail\n");
 
     auto image = create_macbeth_colorchecker_image<Image1D_8U>(DemosaicTypes::COLOR_BayerRGGB2BGR, macbethResolution);
 
-    const auto result = save_image_as_tiff<Image1D_8U>(image, "/home/jaimerios/Pictures/MacbethImageRGGB_RAW.tiff",
-                                                       width, height);
+    auto [bayerImage, bayerPixelsWide, bayerPixelsHigh] = convert_rgb_data_to_bayer_layout(macbethImage, DemosaicTypes::COLOR_BayerRGGB2BGR);
+
+    const auto result = save_image_as_tiff(bayerImage, "/home/jaimerios/Pictures/MacbethImageRGGB_RAW.tiff",
+                                           bayerPixelsWide, bayerPixelsHigh);
 
     return result == true ? 0 : 1;
 }
